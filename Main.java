5ad2aac5f8dde0ee84e348187e7145a0bc30434e
TÖRÖK Attila <torokati44@gmail.com>
2023-06-28 23:09:54 +0200

    Update readme and add support for multiple frames.

--- Main.java
+++ Main.java
@@ -4,43 +4,71 @@ import java.io.File;
 import java.io.FileOutputStream;
 import javax.imageio.ImageIO;
 
-
 public class Main {
+    public static void printUsage() {
+        System.out.println("Usage:\n\njava Main [-v2] frame1.png [frame2.png...] video.flv");
+    }
+
     public static void main(String[] args) {
+        boolean useSVC2 = false;
 
-        ScreenVideoFlvEncoder encoder = new ScreenVideoFlvEncoder();
+        if (args.length > 0 && args[0].equals("-v2")) {
+            useSVC2 = true;
+            args = java.util.Arrays.copyOfRange(args, 1, args.length);
+        }
 
-        boolean useSVC2 = true;
+        if (args.length < 2) {
+            printUsage();
+            System.exit(1);
+        }
+
+        // each must be a multiple of 16, and at most 256
+        int blockWidth = 16;
+        int blockHeight = 16;
 
         try {
-            BufferedImage image = ImageIO.read(new File("test.png"));
+            ScreenVideoFlvEncoder encoder = new ScreenVideoFlvEncoder();
+
+            FileOutputStream fos = new FileOutputStream(args[args.length - 1]);
+            fos.write(encoder.encodeHeader());
 
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            bos.write(ScreenVideoEncoder.encodeFlvVideoDataHeader(true, useSVC2));
-            bos.write(ScreenVideoEncoder.encodeBlockAndScreenDimensions(16, image.getWidth(), 16, image.getHeight()));
-            if (useSVC2)
-                bos.write(new byte[] {0}); // reserved and flags
-            for (int block_bottom = image.getHeight(); block_bottom > 0; block_bottom -= 16) {
-                for (int block_left = 0; block_left < image.getWidth(); block_left += 16) {
-                    int width = Math.min(16, image.getWidth() - block_left);
-                    int height = Math.min(16, block_bottom);
-                    BufferedImage subImage = image.getSubimage(block_left, block_bottom - height, width, height);
-                    int[] pixels = ScreenVideoEncoder.getPixels(subImage, 0, 0, subImage.getWidth(), subImage.getHeight(), useSVC2);
 
-                    if (useSVC2)
-                        bos.write(ScreenVideoEncoder.encodePixelsSVC2(pixels, subImage.getWidth(), subImage.getHeight()));
-                    else
-                        bos.write(ScreenVideoEncoder.encodePixels(pixels, subImage.getWidth(), subImage.getHeight(), false));
+            for (int i = 0; i < args.length - 1; i++) {
+                System.out.println("Encoding " + args[i]);
+                bos.reset();
+                bos.write(ScreenVideoEncoder.encodeFlvVideoDataHeader(true, useSVC2));
+
+                BufferedImage image = ImageIO.read(new File(args[i]));
+                bos.write(ScreenVideoEncoder.encodeBlockAndScreenDimensions(blockWidth, image.getWidth(), blockHeight, image.getHeight()));
+
+                if (useSVC2)
+                    bos.write(new byte[] {0}); // reserved bits and some flags
+
+                for (int blockBottom = image.getHeight(); blockBottom > 0; blockBottom -= blockHeight) {
+                    for (int blockLeft = 0; blockLeft < image.getWidth(); blockLeft += blockWidth) {
+                        int width = Math.min(blockWidth, image.getWidth() - blockLeft);
+                        int height = Math.min(blockHeight, blockBottom);
+
+                        BufferedImage subImage = image.getSubimage(blockLeft, blockBottom - height, width, height);
+
+                        int[] pixels = ScreenVideoEncoder.getPixels(subImage, 0, 0, width, height, useSVC2);
+                        if (useSVC2)
+                            bos.write(ScreenVideoEncoder.encodePixelsSVC2(pixels, width, height));
+                        else
+                            bos.write(ScreenVideoEncoder.encodePixels(pixels, width, height, false));
+                    }
                 }
-            }
 
-            FileOutputStream fos = new FileOutputStream("test.flv");
-            fos.write(encoder.encodeHeader());
-            fos.write(encoder.encodeFlvData(bos.toByteArray()));
+                fos.write(encoder.encodeFlvData(bos.toByteArray()));
+            }
             fos.close();
+
+            System.out.println("Done.");
         }
         catch (Exception e) {
             System.out.println(e);
+            System.exit(2);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,16 +1,27 @@
-Flash Screen Video Encoder
-===
+# Flash Screen Video Encoder
 
 This is a very basic encoder for Flash Screen Video (V1 and V2) files into FLV container.
 
 It doesn't fully utilize the features of the V2 format.
 
-The output can then be embedded into `.swf` if needed, by using:
-
-`$ ffmpeg -i video.flv -vcodec copy -r <FPS> video.swf`
-
-The original files (except for `Main.java`) were salvaged from here, under the LGPL 2.1 license:
+The original files (except for `Main.java` and this README) were salvaged from here, under the GNU LGPL (version 2.1 or 3.0) license:
 
 https://github.com/bigbluebutton/bigbluebutton/tree/c868beed6a6cbf886c6c13ed6bb0c8256507c50a
 
 They were then cleaned up and modified by me.
+
+### Building
+
+`$ javac *.java`
+
+### Running
+
+`$ java Main <args>`
+
+Run without any arguments to see help on usage.
+
+### Converting to SWF
+
+The output, at least V1, can be embedded into an SWF if needed, by running:
+
+`$ ffmpeg -i video.flv -vcodec copy -r <FPS> video.swf`
